59cf723006f4cd9ea55a6d91f38731d1f5c9fa81
Abvorobey <abvorobey@gmail.com>
2023-03-05 19:59:05 +0300

    Замена строк

---
+++ Change_Rows/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задайте двумерный массив. 
+Напишите программу, которая поменяет местами первую и последнюю строку массива.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+
+8 4 2 4
+5 9 2 3
+1 4 7 2
+*/
+using System;
+using static System.Console;
+Clear();
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] ChangeRows(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0, j];
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+    return array;
+}
+
+int[,] array = GetArray(5, 5);
+PrintArray(array);
+WriteLine();
+int[,] array1 = ChangeRows(array);
+PrintArray(array1);
